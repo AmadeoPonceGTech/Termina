4a0c57910461cbb0e5e1f97a54487286e0ed1cd6
lmarcucci <orio60@outlook.fr>
2026-04-03 09:43:48 +0200

    fix merge main into LogManager

--- Sources/GameAssembly/Entities/Enemies/Enemy.h
+++ Sources/GameAssembly/Entities/Enemies/Enemy.h
@@ -1,6 +1,52 @@
 #pragma once
 #include "../Entity.h"
 
+#pragma once
+
+// Common
+#include "../../Artefacts/Common/SmokeBomb.h"
+#include "../../Artefacts/Common/BearSFoot.h"
+#include "../../Artefacts/Common/KnightsBadge.h"
+#include "../../Artefacts/Common/EelsEye.h"
+#include "../../Artefacts/Common/Tail.h"
+#include "../../Artefacts/Common/ToeBone.h"
+#include "../../Artefacts/Common/WolfFur.h"
+
+// Rare
+//#include "../../Artefacts/Rare/ChaoticArmor.h"
+//#include "../../Artefacts/Rare/Honey.h"
+//#include "../../Artefacts/Rare/KnightsHelmet.h"
+//#include "../../Artefacts/Rare/Scale.h"
+//#include "../../Artefacts/Rare/Feather.h"
+//#include "../../Artefacts/Rare/BurningBone.h"
+//#include "../../Artefacts/Rare/RatTooth.h"
+//#include "../../Artefacts/Rare/WolfTooth.h"
+
+// Epic
+#include "../../Artefacts/Epic/MaliciousEye.h"
+#include "../../Artefacts/Epic/BearsSalmon.h"
+#include "../../Artefacts/Epic/CursedShield.h"
+#include "../../Artefacts/Epic/CaspersEncyclopedia.h"
+//#include "../../Artefacts/Epic/MermaidTail.h"
+#include "../../Artefacts/Epic/RattataSkull.h"
+//#include "../../Artefacts/Epic/PackAura.h"
+
+// Legendary
+//#include "../../Artefacts/Legendary/ChaosScepter.h"
+//#include "../../Artefacts/Legendary/GreatBearMedal.h"
+//#include "../../Artefacts/Legendary/DarkSword.h"
+//#include "../../Artefacts/Legendary/Ectoplasm.h"
+//#include "../../Artefacts/Legendary/Seaweed.h"
+//#include "../../Artefacts/Legendary/RatEyeTalisman.h"
+//#include "../../Artefacts/Legendary/ScaleOfTheWhale.h"
+//#include "../../Artefacts/Legendary/EternalFlame.h"
+//#include "../../Artefacts/Legendary/ForestRunes.h"
+//#include "../../Artefacts/Legendary/Humerus.h"
+//#include "../../Artefacts/Legendary/AlphaMedal.h"
+
+// Unique
+//#include "../../Artefacts/Unique/GargoyleSkull.h"
+
 enum class Biome {
     FOREST,
     GRAVEYARD,
