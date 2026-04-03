7964c92ec4a43694daf9352b69d0de67d7c95a1a
lmarcucci <orio60@outlook.fr>
2026-04-03 11:53:40 +0200

    added map for character/artefact link

--- Sources/GameAssembly/Inventory.h
+++ Sources/GameAssembly/Inventory.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <unordered_map>
 #include "Artefacts/Artefact.h"
+#include "Entities/Characters/Character.h"
 
 class Inventory {
 public:
@@ -16,7 +17,16 @@ public:
 
     void checkArtefactsInventory();
 
+    const std::unordered_map<std::shared_ptr<Character>, std::shared_ptr<Artefact>>& getMapCharacterToArtefact() const {
+        return mapCharacterToArtefact;
+    }
+
+    void setMapCharacterToArtefact(const std::unordered_map<std::shared_ptr<Character>, std::shared_ptr<Artefact>>& newMap) {
+        mapCharacterToArtefact = newMap;
+    }
+
 private:
     std::unordered_map<std::string, int> items;
     std::vector<std::shared_ptr<Artefact>> artefactsInventory;
+    std::unordered_map<std::shared_ptr<Character>, std::shared_ptr<Artefact>> mapCharacterToArtefact;
 };
